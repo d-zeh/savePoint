c19e1b89baae661e8a9a5695ed6a26600661da5b
Daniel Zeh <dfzeh1@gmail.com>
2023-09-28 16:02:34 -0500

    login page completed kind of

--- src/main/resources/static/javascript/home.js
+++ src/main/resources/static/javascript/home.js
@@ -1,6 +1,7 @@
-const loginUsername = document.getElementbyId('usernameLogin');
-const loginPassword = document.getElementbyId('passwordLogin');
-const loginForm = document.getElementbyId('loginForm')
+console.log(document);
+const loginUsername = document.getElementById('usernameLogin');
+const loginPassword = document.getElementById('passwordLogin');
+const loginForm = document.getElementById('loginForm')
 
 
 const headers = {
@@ -10,27 +11,40 @@ const headers = {
 const baseUrl = 'http://localhost:8080/api/v1/users'
 
 const handleSubmit = async (e) =>{
-e.preventDefault();
+    console.log("Before the prevent Default");
+    e.preventDefault();
+    console.log("Right after prevent Default");
 
     let bodyObj = {
     username: loginUsername.value,
     password: loginPassword.value
     }
 
+
     const response = await fetch(`${baseUrl}/login`,{
     method: "POST",
     body: JSON.stringify(bodyObj),
     headers: headers
     })
-    .catch(err => console.error(err.message))
 
     const responseArr = await response.json()
 
     if(response.status == 200) {
+    console.log("This is connected");
+    if(responseArr[0] == "User Login Successful") {
+    alert(responseArr[0])
     document.cookie = `userId=${responseArr[1]}`
     window.location.replace(responseArr[0])
+
     }
-}
+
+
+    } else {
+    alert("Oh oh! Your username or password was incorrect. Get fucked!")
+    }
+    }
+
+
 
 loginForm.addEventListener("submit", handleSubmit)
 
